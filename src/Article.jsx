5e33755501f97c9fbc1d84261d8b79c6a2a0ffb0
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-04-22 12:43:50 +0400

    format date with date-fns

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import styles from "./Article.module.css";
+import { format } from "date-fns";
 
-function Article({article}) {
+function Article({article}) { 
+    const date = new Date(article.publishedAt);
+    const formattedDate = format(date, "MM/dd/yyyy");
+
   return (
     <div className={styles.article}>
 
-        <img src={article.urlToImage} alt="article-photo" />
-        
+        <img src={article.urlToImage ? article.urlToImage : "https://storage.googleapis.com/stenbracka/public/placeholder.jpg"} alt="article-photo" />
+
         <h3>{article.title}</h3>
 
         <p className={styles.description}>{article.description}</p>
 
         <div className={styles.authorAndDate}>
             <p>{article.author}</p>
-            <p className={styles.date}>01/01/2023</p>
+            <p className={styles.date}>{formattedDate}</p>
         </div>
-
+        
     </div>
   );
 }
