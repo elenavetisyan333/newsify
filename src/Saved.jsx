ccf2c930f7670ac3d19f7994d35040b6ccb49c04
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-04-26 13:22:13 +0400

    add modal to news in the saved page

--- src/Saved.jsx
+++ src/Saved.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import styles from "./Home.module.css";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import Article from "./Article";
+import Modal from "./Modal";
 
 function Saved() {
     const savedArticles = useSelector(store => store.savedNews.savedNews);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedArticle, setSelectedArticle] = useState({});
+  
+    function handleArticleClick(article){
+      setSelectedArticle(article);
+      setIsModalOpen(true);
+    }
+  
+    function handleCloseModal(){
+      setSelectedArticle({});
+      setIsModalOpen(false);
+    }
 
     return (
         <div className={styles.homePage}>
@@ -14,11 +29,14 @@ function Saved() {
                         return <Article 
                                     article={article}  
                                     key={`article-${i}`}
+                                    onClick={() => handleArticleClick(article)}
                                 />;
                     })
                 }
             </div>
-          
+            {isModalOpen && (
+                <Modal isOpen={isModalOpen} article={selectedArticle} onClose={handleCloseModal} />
+            )}
         </div>
     );
 }
