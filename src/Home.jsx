b0194c2cae885aca1d72eab1a0d520977fb9c0df
elenavetisyan333 <elen.avetisyan.333@mail.ru>
2023-04-28 01:17:03 +0400

    add countries part

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,20 +8,21 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import Article from "./Article";
 import Modal from "./Modal";
-// import { countries } from "./countries";
+import { countries } from "./countries";
 
 function Home() {
     const API_KEY = "eb88c94ff5c0403dbab88f7a05913667";
     const articles = useSelector(store => store.news.news);
     const categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState("us");
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedArticle, setSelectedArticle] = useState({});
     const dispatch = useDispatch();
 
     async function getNews(){
         const news = await axios
-                            .get(`https://newsapi.org/v2/top-headlines?country=us&category=${selectedCategory}&apiKey=${API_KEY}`)
+                            .get(`https://newsapi.org/v2/top-headlines?country=${selectedCountry}&category=${selectedCategory}&apiKey=${API_KEY}`)
                             .then(rsp => rsp.data.articles);
         
         dispatch(setNews(news));
@@ -29,7 +30,7 @@ function Home() {
     }
     useEffect(()=>{
         getNews();
-    },[selectedCategory]);
+    },[selectedCategory, selectedCountry]);
   
     function handleArticleClick(article){
       setSelectedArticle(article);
@@ -43,7 +44,7 @@ function Home() {
 
     return (
         <div className={styles.content}>
-            <div className="countriesAndCategories">
+            <div className={styles.categoriesAndCountries}>
                 <div className={styles.categories}>
                     {
                         categories.map((category) =>{
@@ -53,6 +54,18 @@ function Home() {
                         })
                     }                       
                 </div>
+
+                
+                <select className={styles.countries} onChange={(e) => setSelectedCountry(e.target.value)}>
+                    <option value="us">Select a Country</option>;
+                    {
+                        countries.map((country) =>{
+                            return (
+                                <option key={`country-${country.name}`} value={country.code}>{country.name}</option>
+                            );
+                        })
+                    }
+                </select>
             </div>
 
             <div className={styles.homePage}>
